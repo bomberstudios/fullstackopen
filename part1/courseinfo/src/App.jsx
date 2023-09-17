8225d16a02e49e6bca18e2e6162a79b7bec04e99
Ale M <bomberstudios@gmail.com>
2023-09-17 11:52:57 +0200

    Solution to exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,31 @@
 const Header = (props) => <h1>{props.title}</h1>
-const Content = (props) => <p>{props.name} {props.exercises}</p>
+const Content = (props) => {
+  const list = props.parts.map((part, index) => {
+    return <Part key={index} name={part.name} exercises={part.exercises} />
+  })
+  return (
+    list
+  )
+}
+
+const Part = (props) => <p>{props.name} {props.exercises}</p>
 const Total = (props) => <p>Number of exercises {props.count}</p>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  }
 
   return (
     <div>
-      <Header title={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
-      <Total count={exercises1 + exercises2 + exercises3} />
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total count={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
