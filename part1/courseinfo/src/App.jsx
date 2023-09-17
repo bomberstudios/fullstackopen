e2511e7a51be14c2921574aa1159cb1f91489a0e
Ale M <bomberstudios@gmail.com>
2023-09-17 20:56:07 +0200

    Exercises 1.3, 1.4 and 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,15 +1,21 @@
-const Header = (props) => <h1>{props.title}</h1>
-const Content = (props) => {
-  const list = props.parts.map((part, index) => {
+/* eslint-disable react/prop-types */
+const Header = ({title}) => <h1>{title}</h1>
+const Content = ({parts}) => {
+  const list = parts.map((part, index) => {
     return <Part key={index} name={part.name} exercises={part.exercises} />
   })
   return (
     list
   )
 }
-
-const Part = (props) => <p>{props.name} {props.exercises}</p>
-const Total = (props) => <p>Number of exercises {props.count}</p>
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+const Total = ({parts}) => {
+  let totalExercises = 0
+  parts.forEach((part) => {
+    totalExercises += part.exercises
+  })
+  return (<p>Number of exercises {totalExercises}</p>)
+}
 
 const App = () => {
   const course = {
@@ -25,7 +31,7 @@ const App = () => {
     <div>
       <Header title={course.name} />
       <Content parts={course.parts} />
-      <Total count={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+      <Total parts={course.parts} />
     </div>
   )
 }
