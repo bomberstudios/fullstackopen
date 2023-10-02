79f1738d820b9f0f9ecebf3ca4087eb91041f21c
Ale M <bomberstudios@gmail.com>
2023-10-02 21:27:04 +0200

    Exercise 2.2

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,6 +1,10 @@
 const Total = ({ parts }) => {
   const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p>Number of exercises {totalExercises}</p>
+  return (
+    <p>
+      <b>Number of exercises {totalExercises}</b>
+    </p>
+  )
 }
 
 export default Total
