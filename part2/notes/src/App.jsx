f8989560f962a909f6d7abdd850e0d5d661a143c
Ale M <bomberstudios@gmail.com>
2023-10-22 22:01:25 +0200

    Use axios and json-server for notes app

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import "./App.css"
 import Note from "./components/Note"
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("A new note…")
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/notes").then((response) => {
+      setNotes(response.data)
+    })
+  }, [])
 
   const visibleNotes = showAll ? notes : notes.filter((note) => note.important)
 
